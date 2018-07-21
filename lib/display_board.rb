d0e3392797e200daebc18d299a94f4e037e8ec38
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-21 16:38:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+def display_board(board)
+  demarcation_line = "-" * 11
+  puts " #{board[0]} |  #{board[1]} | #{board[2]} "
+  puts ("")
+end
