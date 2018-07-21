66fc0e574bd9b6c04e9311eeba1616308176459f
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-21 16:39:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,5 +4,8 @@
 def display_board(board)
   demarcation_line = "-" * 11
   puts " #{board[0]} |  #{board[1]} | #{board[2]} "
-  puts ("")
+  puts "#{demarcation_line}"
+  puts " #{board[3]} |  #{board[4]} | #{board[5]} "
+  puts "#{demarcation_line}"
+  puts " #{board[6]} |  #{board[7]} | #{board[8]} "
 end
